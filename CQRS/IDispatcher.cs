bb3fe3303a1935439fee035b7f0cbd7d4a58a89d
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-05-26 10:41:45 +0200

    Add IDispatcher interface

---
+++ CQRS/IDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CQRS
+{
+    public interface IDispatcher
+    {
+        TResult Dispatch<TResult>(IOperation<TResult> command);
+    }
+}
