2dd34fb76501902c4b534e22f062d0a1f42a8b52
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-05-26 13:27:13 +0200

    Add HandlersLookup with tests

---
+++ CQRS.Tests/HandlersLookupTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CQRS.Tests
+{
+    public sealed class HandlersLookupTests
+    {
+        [Fact]
+        public void ReturnsZeroHandlers()
+        {
+            var command = new Command0();
+            var handler = new TestHandlersLookup().Handler(command);
+            Assert.Empty(handler);
+        }
+
+        [Fact]
+        public void ReturnsOneHandlers()
+        {
+            var command = new Command1();
+            var handler = new TestHandlersLookup().Handler(command);
+            Assert.Single(handler);
+        }
+
+        [Fact]
+        public void ReturnsTwoHandlers()
+        {
+            var command = new Command2();
+            var handler = new TestHandlersLookup().Handler(command);
+            Assert.Equal(2, handler.Count());
+        }
+
+        public sealed class TestHandlersLookup : HandlersLookup
+        {
+            protected override IEnumerable<CommandDefinition> CommandHandlers()
+            {
+                yield return CommandHandler((Command1 x) => new CommandHandler1());
+                yield return CommandHandler((Command2 x) => new CommandHandler2());
+                yield return CommandHandler((Command2 x) => new CommandHandler2());
+            }
+
+            protected override IEnumerable<QueryDefinition> QueryHandlers()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public sealed class Command0 : ICommand<object>
+        {
+        }
+
+        public sealed class CommandHandler0 : ICommandHandler<Command0, object>
+        {
+            public object Handle(Command0 operation)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public sealed class Command1 : ICommand<object>
+        {
+        }
+
+        public sealed class CommandHandler1 : ICommandHandler<Command1, object>
+        {
+            public object Handle(Command1 operation)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public sealed class Command2 : ICommand<object>
+        {
+        }
+
+        public sealed class CommandHandler2 : ICommandHandler<Command2, object>
+        {
+            public object Handle(Command2 operation)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ CQRS/HandlersLookup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CQRS
+{
+    public abstract class HandlersLookup : IHandlersLookup
+    {
+        public IEnumerable<Func<ICommand<TResult>, TResult>> Handler<TResult>(ICommand<TResult> command) =>
+            CommandHandlers()
+                .Where(x => x.MatchType(command.GetType()))
+                .Select(x => x.Handler<TResult>());
+
+        public IEnumerable<Func<IQuery<TResult>, TResult>> Handler<TResult>(IQuery<TResult> query) =>
+            QueryHandlers()
+                .Where(x => x.MatchType(query.GetType()))
+                .Select(x => x.Handler<TResult>());
+
+        protected abstract IEnumerable<CommandDefinition> CommandHandlers();
+
+        protected abstract IEnumerable<QueryDefinition> QueryHandlers();
+
+        protected CommandDefinition CommandHandler<TCommand, TResult>(Func<TCommand, ICommandHandler<TCommand, TResult>> factory)
+            where TCommand : ICommand<TResult>
+        {
+            return new CommandDefinition(
+                typeof(TCommand),
+                command => factory((TCommand)command).Handle((TCommand)command));
+        }
+
+        protected QueryDefinition QueryHandler<TQuery, TResult>(Func<TQuery, IQueryHandler<TQuery, TResult>> factory)
+            where TQuery : IQuery<TResult>
+        {
+            return new QueryDefinition(
+                typeof(TQuery),
+                x => factory((TQuery)x).Handle((TQuery)x));
+        }
+
+        protected sealed class CommandDefinition
+        {
+            private readonly Type _type;
+            private readonly Func<object, object> _handler;
+
+            public CommandDefinition(Type type, Func<object, object> handler)
+            {
+                _type = type;
+                _handler = handler;
+            }
+
+            public bool MatchType(Type type) => Equals(_type, type);
+
+            public Func<ICommand<TResult>, TResult> Handler<TResult>() => x => (TResult)_handler(x);
+        }
+
+        protected sealed class QueryDefinition
+        {
+            private readonly Type _type;
+            private readonly Func<object, object> _handler;
+
+            public QueryDefinition(Type type, Func<object, object> handler)
+            {
+                _type = type;
+                _handler = handler;
+            }
+
+            public bool MatchType(Type type) => Equals(_type, type);
+
+            public Func<IQuery<TResult>, TResult> Handler<TResult>() => x => (TResult)_handler(x);
+        }
+    }
+}
