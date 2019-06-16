454db4580febf18cdb31888dac2bf7fc76aceba1
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-06-16 11:08:56 +0200

    Expose type as property in query and command definitions

--- CQRS/HandlersLookup.cs
+++ CQRS/HandlersLookup.cs
@@ -9,12 +9,12 @@ namespace CQRS
     {
         public IEnumerable<Func<ICommand<TResult>, TResult>> Handler<TResult>(ICommand<TResult> command) =>
             CommandHandlers()
-                .Where(x => x.MatchType(command.GetType()))
+                .Where(x => x.Type.Equals(command.GetType()))
                 .Select(x => x.Handler<TResult>());
 
         public IEnumerable<Func<IQuery<TResult>, TResult>> Handler<TResult>(IQuery<TResult> query) =>
             QueryHandlers()
-                .Where(x => x.MatchType(query.GetType()))
+                .Where(x => x.Type.Equals(query.GetType()))
                 .Select(x => x.Handler<TResult>());
 
         protected abstract IEnumerable<CommandDefinition> CommandHandlers();
@@ -39,32 +39,30 @@ namespace CQRS
 
         protected sealed class CommandDefinition
         {
-            private readonly Type _type;
             private readonly Func<object, object> _handler;
 
             public CommandDefinition(Type type, Func<object, object> handler)
             {
-                _type = type;
+                Type = type;
                 _handler = handler;
             }
 
-            public bool MatchType(Type type) => Equals(_type, type);
+            public Type Type { get; }
 
             public Func<ICommand<TResult>, TResult> Handler<TResult>() => x => (TResult)_handler(x);
         }
 
         protected sealed class QueryDefinition
         {
-            private readonly Type _type;
             private readonly Func<object, object> _handler;
 
             public QueryDefinition(Type type, Func<object, object> handler)
             {
-                _type = type;
+                Type = type;
                 _handler = handler;
             }
 
-            public bool MatchType(Type type) => Equals(_type, type);
+            public Type Type { get; }
 
             public Func<IQuery<TResult>, TResult> Handler<TResult>() => x => (TResult)_handler(x);
         }
