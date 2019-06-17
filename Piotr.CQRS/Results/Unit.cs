6b1bc138b98646b959704368e5b8a5269ba821e0
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-06-17 15:30:03 +0200

    Add Unit result

---
+++ Piotr.CQRS/Results/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piotr.CQRS.Results
+{
+    /// <summary>
+    /// The Unit type indicates the absence of a specific value.
+    /// It's a useful command result.
+    /// </summary>
+    public struct Unit
+    {
+    }
+}
